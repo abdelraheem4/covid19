755f940e18794708bb71f0b521fae76ed849061b
AhmadBaniYaseen <ahmadbaniyassen8@gmail.com>
2022-12-10 17:48:36 +0300

    Ahmad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
 import { CenterComponent } from './center/center.component';
+import { DoctorModule } from './doctor/doctor.module';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path:'admin',
     loadChildren:()=>AdminModule
+  },
+  {
+     path:'managedoctor',
+     loadChildren:()=>DoctorModule
   }
 ];
 
